b7ff8044bd54dd7efb46ef2c267a4f252ed8b8b1
Jeremy <j.leegitau@gmail.com>
2024-11-01 12:17:41 +0300

    added animations

--- app/src/main/java/com/example/wuxia/Kunlun.kt
+++ app/src/main/java/com/example/wuxia/Kunlun.kt
@@ -1,5 +1,14 @@
 package com.example.wuxia
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +20,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
@@ -29,14 +40,40 @@ import com.example.wuxia.model.Sage
 import com.example.wuxia.model.SageSource
 import com.example.wuxia.ui.theme.WuxiaTheme
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun SageList(sage: List<Sage>, modifier: Modifier=Modifier,contentPadding: PaddingValues =PaddingValues(0.dp)){
-    LazyColumn(contentPadding=contentPadding) {
-        items(sage.size) {
-            SageItemCard(
-                sage = sage[it],
-                modifier = modifier.padding(8.dp)
-            )
+fun SageList(sage: List<Sage>, modifier: Modifier=Modifier,contentPadding: PaddingValues =PaddingValues(0.dp)
+){
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            targetState = true
+        }
+    }
+
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = fadeIn(
+            animationSpec = spring(dampingRatio = DampingRatioLowBouncy)
+        ),
+        exit = fadeOut(),
+        modifier = modifier
+    ) {
+        LazyColumn(contentPadding = contentPadding) {
+            itemsIndexed(sage) { idx, sage ->
+                SageItemCard(
+                    sage = sage,
+                    modifier = modifier.padding(6.dp)
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                animationSpec = spring(
+                                    stiffness = StiffnessVeryLow,
+                                    dampingRatio = DampingRatioLowBouncy
+                                ),
+                                initialOffsetY = {it * (idx +1)}
+                            )
+                        )
+                )
+            }
         }
     }
 }
@@ -46,12 +83,11 @@ fun SageItemCard(sage: Sage,modifier: Modifier = Modifier){
     Card(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         modifier = modifier){
-        Row(modifier = modifier.sizeIn(minHeight = 72.dp).padding(16.dp)) {
+        Row(modifier = modifier.padding(12.dp).sizeIn(minHeight = 72.dp)) {
             Column(modifier = modifier.weight(1f)) {
                 Text(
                     text = stringResource(sage.name),
                     style = MaterialTheme.typography.displaySmall
-
                 )
                 Text(
                     text = stringResource(sage.desc),
